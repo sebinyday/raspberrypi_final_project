1d6a3d0ca10cc268a69ba4905d5a3c2d5a6e5e55
sebinyday <jjnf0219@gmail.com>
2024-12-08 14:47:44 +0900

    RGB_LED.py final version

--- RGB_LED.py
+++ RGB_LED.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-RGB_PINS = {'R': 26, 'G': 6, 'B': 13}
+RGB_PINS = {'R': 26, 'G': 12, 'B': 13}
 
 def setup_rgb_pins():
     GPIO.setup(list(RGB_PINS.values()), GPIO.OUT, initial=GPIO.LOW)
-    
-# 각 색깔을 켜는 함수들
+
+# 각 색깔을 켜고 끄는 함수들
 def red_led_on():
     print("Red LED ON")
     GPIO.output(RGB_PINS['R'], 1)
@@ -31,7 +31,7 @@ def purple_led_off():
     GPIO.output(RGB_PINS['R'], 0)
 
 def yellow_led_on():
-    print("Yellow LED ON")  # 노란색은 빨강 + 초록
+    print("Yellow LED ON") 
     GPIO.output(RGB_PINS['R'], 1)
     GPIO.output(RGB_PINS['G'], 1)
 
@@ -44,23 +44,3 @@ def all_led_off():
     GPIO.output(RGB_PINS['G'], 0)
     GPIO.output(RGB_PINS['B'], 0)
 
-# try:
-#     while True:
-#         red_led_on()
-#         time.sleep(1)
-#         red_led_off()
-
-#         green_led_on()
-#         time.sleep(1)
-#         green_led_off()
-
-#         blue_led_on()
-#         time.sleep(1)
-#         blue_led_off()
-
-#         yellow_led_on()
-#         time.sleep(1)
-#         yellow_led_off()
-
-# except KeyboardInterrupt:
-#     GPIO.cleanup()

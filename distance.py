a180807374f44f6e9117782545e0a51bc8e795de
sebinyday <jjnf0219@gmail.com>
2024-12-08 14:35:40 +0900

    distance.py final version

--- distance.py
+++ distance.py
@@ -4,21 +4,18 @@ import time
 TRIG = 23  # 초음파 트리거 핀
 ECHO = 24  # 초음파 에코 핀
 
-
 def setup_distance_pin():
     GPIO.setup(TRIG, GPIO.OUT)
     GPIO.setup(ECHO, GPIO.IN)
-    
-    
+      
 def measure_distance():
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
-    GPIO.output(TRIG, False)
+    GPIO.output(TRIG, False) 
     start, stop = 0, 0
     while GPIO.input(ECHO) == 0:
         start = time.time()
     while GPIO.input(ECHO) == 1:
         stop = time.time()
-    return int((stop - start) * 34300 / 2)  # cm 단위로 정수로 반환
-
-
+    distance = int((stop - start) * 34300 / 2)
+    return distance
